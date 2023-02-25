7583050ecacb00445646a2e73817ec372b522fcc
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-02-25 06:45:57 +0600

    complate food resturant

---
+++ food.js
@@ -0,0 +1,79 @@
+
+// call the dayanamic api
+const locatedFood = (searchText)=>{
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+  console.log(url)
+  fetch(url)
+  .then(res=>res.json())
+  .then(data=>dispalyFood(data.meals))
+}
+
+// work the food card 
+const dispalyFood = meals =>{
+  const cardContainer = document.getElementById('card-container')
+  cardContainer.innerText = '';
+  meals.forEach(meal => {
+    console.log(meal)
+    const creatDiv = document.createElement('div')
+    creatDiv.classList.add('col', 'para')
+     
+    creatDiv.innerHTML = `
+
+    <div class="card h-auto w-100">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body ">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text  para">${meal.strInstructions}</p>
+
+      <button onclick = "loadMealDetails(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailsModal">
+        MealDetails
+      </button>
+
+    </div>
+  </div> 
+ `
+    cardContainer.appendChild(creatDiv)
+  });
+}
+
+// working the search button
+function searchbtn() {
+  const  searchText = document.getElementById('search-input').value;
+   console.log(searchText)
+   locatedFood(searchText)
+}
+
+// async use to food details
+const loadMealDetails =async (idMeal) =>{
+  const Url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+   try{
+    const collectUrl = await fetch(Url)
+    const Data = await collectUrl.json()
+    displayModal(Data.meals[0]) 
+   }
+   catch(error){
+    console.log(error)
+   }
+}
+
+// arrow function use to food detials
+
+// const loadMealDetails = idMeal =>{
+//   const Url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+//   fetch(Url)
+//   .then(Response=>Response.json())
+//   .then(Data => displayModal(Data.meals[0]) )
+// }
+
+// display the food details
+const displayModal = meal =>
+{
+  document.getElementById('detailsModalLabel').innerText = meal.strMeal;
+  const mealDetails =  document.getElementById('mealDetails')
+  mealDetails.innerHTML = `
+  <img class = "img-fluid mb-" src="${meal.strMealThumb}"  alt="...">
+   <p>${meal.strInstructions}</p>
+  `;
+}
+
+locatedFood('');
